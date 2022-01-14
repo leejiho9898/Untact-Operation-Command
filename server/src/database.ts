2c4bb36dc3c91355037381bb12f22e8f8fe2563b
leejiho9898 <84016039+leejiho9898@users.noreply.github.com>
2022-01-14 16:36:20 +0900

    Update database.ts

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 const DBURL =
-  process.env.DBURL ||
-  "mongodb+srv://admin:test123@cafenual.3wvli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  process.env.DBURL
 
 mongoose
   .connect(DBURL, {
